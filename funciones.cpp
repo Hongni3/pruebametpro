5e071f94440cc6b839de7021a13c46168cc9269d
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-19 07:53:01 -0600

    Agregue el archivo funciones.cpp

---
+++ funciones.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+#define MAX = 10;
+
+//arreglo
+
+int edades[10]; 
+int pos=0; //posicion de elemento 
+
+void agregarEdad(int edad);
+bool hayEspacio();
+
+void agregarEdad(int edad){
+    edades[pos] = edad;
+    pos++;
+}
+
+bool hayEspacio(){
+    return pos< MAX; 
+}
